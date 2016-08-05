fad976e7277e198996166b6df374b07688491d9d
Ruby Byrne <rubylinderbyrne@gmail.com>
2016-08-04 22:27:51 -0400

    added ra/dec plotting code

--- data_deleter.py
+++ data_deleter.py
@@ -3,11 +3,12 @@
 #Script that deletes downloaded data based on their cotter version and subverion numbers
 
 import os
+import time
 
 def main():
 
-	version = 0
-	subversion = 0
+	version = 3
+	subversion = 4
 
 	all_nodes = ["eor-00", "eor-02", "eor-03", "eor-04", "eor-05", "eor-06", "eor-07", "eor-08", "eor-09", "eor-10", "eor-11", "eor-12", "eor-13", "eor-14"]
 	all_nodes = ["/nfs/" + nodename + "/r1/EoRuvfits/" for nodename in all_nodes]
@@ -19,7 +20,8 @@ def main():
 			for dirname in directory_contents:
 				if dirname.endswith("v" + str(version) + "_" + str(subversion)):
 					print dirname + " FOUND IN DIRECTORY " + node_path + " ... DELETING ..."
-					os.system("rm " + node_path + dirname + " -r")
+					time.sleep(0)
+					os.system("rm -f " + node_path + dirname + " -r")
 					dir_deleted = True
 	if not dir_deleted:
 		print "NO FILES FOUND"
