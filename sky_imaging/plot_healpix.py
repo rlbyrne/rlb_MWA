bea63b74aba6ca6462a8368e95950c176c5a6712
rudyab <rubylinderbyrne@gmail.com>
2017-12-01 12:40:48 -0800

    new healpix tiling code

--- sky_imaging/plot_healpix.py
+++ sky_imaging/plot_healpix.py
@@ -11,10 +11,12 @@ import healpy as hp
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
-import sys
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from scipy.interpolate import griddata
+import sys
+sys.path.insert(0, '/Users/ruby/EoR/rlb_MWA/diffuse_survey_coverage_visualization/')
+import surveyview
 
 
 def healpix_converter(data_filename):
@@ -62,16 +64,201 @@ def healpix_converter(data_filename):
     plt.show()
 
 
-def plot_filled_pixels(data_filename, save_filename):
+def plot_healpix_file(data_filename, save_filename):
 
-    data = load_map(data_filename)
+    data, nside, nest = load_map(data_filename)
+    plot_filled_pixels(data, nside, nest, save_filename)
+
+
+def plot_healpix_tiling():
+
+    data_type = 'Residual_I'
+    normalization = 'uniform'
+    data_dir = '/Users/ruby/EoR/Healpix_fits'
+
+    tile_center_ras = [100, 100, 100, 100, 100,
+                       90, 90, 90, 90, 90,
+                       80, 80, 80, 80, 80,
+                       70, 70, 70, 70, 70,
+                       60, 60, 60, 60, 60,
+                       50, 50, 50, 50, 50,
+                       40, 40, 40, 40, 40,
+                       30, 30, 30, 30, 30,
+                       20, 20, 20, 20, 20,
+                       10, 10, 10, 10, 10,
+                       0, 0, 0, 0, 0,
+                       -10, -10, -10, -10, -10,
+                       -20, -20, -20, -20, -20]
+
+    tile_center_decs = [-5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45,
+                        -5, -15, -25, -35, -45]
+
+    #obsids = [1131478056, 1131472536, 1131468936, 1131463536, 1131717352,
+    #          1131711952, 1131535544,
+    #          1131477936, 1131472416, 1131468816, 1131463416, 1131457896,
+    #          1131454296, 1131710032,
+    #          1131477816, 1131731632, 1130778424, 1131463296, 1131459576,
+    #          1131713512, 1131709912]
+
+    obsids = [1131478056, 1131564464, 1131477936, 1130787784, 1131477816,
+              1131733672, 1131562544, 1131733552, 1130785864, 1131475896,
+              1131559064, 1131558944, 1131731752, 1130782264, 1130783824,
+              1131470736, 1131557144, 1131470616, 1130780464, 1131470496,
+              1131468936, 1131553544, 1131726352, 1130778664, 1131468696,
+              1131724672, 1131551744, 1131465216, 1130776864, 1130776624,
+              1131463536, 1131549944, 1131463416, 1130773264, 1131463296,
+              1131461616, 1131548024, 1131461496, 1130773144, 1131461376,
+              1131717352, 1131544424, 1131717232, 1131459696, 1131459576,
+              1131456216, 1131542624, 1131456096, 1131715432, 1131715312,
+              1131711952, 1131540824, 1131454296, 1131713632, 1131454176,
+              1131710152, 1131537224, 1131710032, 1131710032, 1131709912,
+              1131535544, 1131535424, 1131535304, 1131710032, 1131709912]
+
+
+    data = []
+    for i, obs in enumerate(obsids):
+        obs_data, nside, nest = load_map('{}/{}_{}_{}_HEALPix.fits'.format(data_dir, obs, normalization, data_type))
+        tile_bounds_radec = [[tile_center_ras[i]-5, tile_center_decs[i]-5],
+                             [tile_center_ras[i]-5, tile_center_decs[i]+5],
+                             [tile_center_ras[i]+5, tile_center_decs[i]+5],
+                             [tile_center_ras[i]+5, tile_center_decs[i]-5]]
+        tile_bounds_vec = np.array([hp.pixelfunc.ang2vec(corner[0], corner[1], lonlat=True) for corner in tile_bounds_radec])
+        use_pixels = hp.query_polygon(nside, tile_bounds_vec, nest=nest)
+        data.extend([data_point for data_point in obs_data if data_point.pixelnum in use_pixels])
+
+    fig, ax = plt.subplots(figsize=(12, 6), dpi=2000)
+
+    # Collect Healpix pixels to plot
+    patches = []
+    colors = []
+    for point in data:
+        point.get_pixel_corners(nside, nest)
+        polygon = Polygon(zip(point.pix_corner_ras, point.pix_corner_decs))
+        patches.append(polygon)
+        colors.append(point.signal)
+
+    collection = PatchCollection(patches, cmap='Greys_r', lw=0.04)
+    collection.set_array(np.array(colors))  # set the data colors
+    collection.set_edgecolor('face')  # make the face and edge colors match
+    ax.add_collection(collection)  # plot data
+
+    # plot lines between tiles
+    line_width = 2.0
+    color = 'gray'
+    order = 8
+    plt.plot([130, -45], [-10, -10], lw=line_width, c=color, zorder=order)
+    plt.plot([130, -45], [-20, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([130, -45], [-30, -30], lw=line_width, c=color, zorder=order)
+    plt.plot([130, -45], [-40, -40], lw=line_width, c=color, zorder=order)
+    plt.plot([130, -45], [-50, -50], lw=line_width, c=color, zorder=order)
+    plt.plot([95, 95], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([85, 85], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([75, 75], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([65, 65], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([55, 55], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([45, 45], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([35, 35], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([25, 25], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([15, 15], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([5, 5], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([-5, -5], [-75, -20], lw=line_width, c=color, zorder=order)
+    plt.plot([-15, -15], [-75, -20], lw=line_width, c=color, zorder=order)
+
+    plt.xlabel('RA (deg)')
+    plt.ylabel('Dec (deg)')
+    plt.axis('equal')
+    ax.set_facecolor('black')  # make plot background black
+    plt.axis([130, -45, -75, 20])
+    plt.grid(which='both', lw=1.0, zorder=10)
+    cbar = fig.colorbar(collection, ax=ax)  # add colorbar
+    cbar.ax.set_ylabel('Flux Density (Jy/sr)', rotation=270)  # label colorbar
+
+    plt.savefig('/Users/ruby/EoR/Healpix_fits/mosaicplot.png', format='png', dpi=2000)
+
+
+def plot_healpix_mosaic(data_dir, obs_array, save_filename):
+
+    data_type = 'Residual_I'
+    normalization = 'uniform'
+    obs_info_file = '/Users/ruby/EoR/sidelobe_survey_obsinfo.txt'
+    boundary_width = 0.1
+
+    if data_dir.endswith('/'):
+        data_dir = data_dir[:-1]
+
+    observations = surveyview.load_survey(obs_info_file)
+    observations = [obs for obs in observations if obs.obsid in obs_array]
+
+    data_files = os.listdir(data_dir)
+    for i, use_obsid in enumerate(obs_array):
+        obsid_list = [obs.obsid for obs in observations]
+        heal_file = '{}_{}_{}_HEALPix.fits'.format(use_obsid, normalization,
+                                                   data_type)
+        if heal_file in data_files:
+            observations[obsid_list.index(use_obsid)].heal_data, \
+                nside_new, nest_new \
+                = load_map('{}/{}'.format(data_dir, heal_file))
+            if i != 0:
+                if nside_new != nside:
+                    print 'ERROR: HEALPix nsides do not match. Exiting.'
+                    sys.exit(1)
+                if nest_new != nest:
+                    print 'ERROR: HEALPix ordering conventions do not match. Exiting.'
+                    sys.exit(1)
+            nside = nside_new
+            nest = nest_new
+        else:
+            print 'WARNING: Data file {} not found.'.format(heal_file)
+            del observations[obsid_list.index(use_obsid)]
+
+    pixel_vals = []
+    for obs in observations:
+        pixel_vals.extend(
+            [data_point.pixelnum for data_point in obs.heal_data]
+            )
+    pixel_vals = list(set(pixel_vals))
+
+    use_data = [0]*len(pixel_vals)
+    for i, pixel in enumerate(pixel_vals):
+        if i % 100 == 0:
+            print i
+        pixel_dist = [float('inf')]*len(observations)
+        pixel_ra, pixel_dec = hp.pixelfunc.pix2ang(nside, pixel, nest=nest,
+                                                   lonlat=True)
+        for j, obs in enumerate(observations):
+            if pixel in [data_point.pixelnum for data_point in obs.heal_data]:
+                pixel_dist[j] = (obs.ra-pixel_ra)**2 + (obs.dec-pixel_dec)**2
+
+        pixel_dist_sorted = sorted(pixel_dist)
+        obs_use_data = observations[pixel_dist.index(pixel_dist_sorted[0])].heal_data
+        use_data[i] = obs_use_data[([data_point.pixelnum for data_point in obs_use_data]).index(pixel)]
+        if pixel_dist_sorted[1]-pixel_dist_sorted[0] < boundary_width**2:
+            use_data[i].signal = 0
+
+    print nside
+    plot_filled_pixels(use_data, nside, nest, save_filename)
+
+
+def plot_filled_pixels(data, nside, nest, save_filename):
+
     fig, ax = plt.subplots()
 
     # Collect Healpix pixels to plot
     patches = []
     colors = []
     for point in data:
-        point.get_pixel_corners()
+        point.get_pixel_corners(nside, nest)
         polygon = Polygon(zip(point.pix_corner_ras, point.pix_corner_decs))
         patches.append(polygon)
         colors.append(point.signal)
@@ -94,8 +281,17 @@ def plot_filled_pixels(data_filename, save_filename):
 def load_map(data_filename):
 
     contents = fits.open(data_filename)
-    nside = contents[1].header['nside']
+    nside = int(contents[1].header['nside'])
     ordering = contents[1].header['ordering']
+
+    if ordering.lower() == 'ring':
+        nest = False
+    elif ordering.lower() == 'nested':
+        nest = True
+    else:
+        print 'ERROR: Invalid ordering parameter.'
+        print 'Ordering must be "ring" or "nested". Exiting.'
+        sys.exit(1)
 
     data = contents[1].data
     pixel_vals = data.field('PIXEL')
@@ -107,42 +303,31 @@ def load_map(data_filename):
 
     pixel_data = []
     for i in range(len(pixel_vals)):
-        data_point = HealpixPixel(nside, pixel_vals[i], ordering,
-                                  signal_vals[i])
-        data_point.get_ra_dec()
+        data_point = HealpixPixel(pixel_vals[i], signal_vals[i])
         pixel_data.append(data_point)
 
-    return pixel_data
+    return pixel_data, nside, nest
 
 
 class HealpixPixel:
 
-    def __init__(self, nside, pixelnum, ordering, signal):
-        self.nside = int(nside)
+    def __init__(self, pixelnum, signal):
         self.pixelnum = int(pixelnum)
-        if ordering.lower() == 'ring':
-            self.nest = False
-        elif ordering.lower() == 'nested':
-            self.nest = True
-        else:
-            print 'ERROR: Invalid ordering parameter.'
-            print 'Ordering must be "ring" or "nested". Exiting.'
-            sys.exit(1)
         self.signal = float(signal)
 
-    def get_ra_dec(self):
-        ra, dec = hp.pixelfunc.pix2ang(self.nside, self.pixelnum,
-                                       nest=self.nest, lonlat=True)
+    def get_ra_dec(self, nside, nest):
+        ra, dec = hp.pixelfunc.pix2ang(nside, self.pixelnum,
+                                       nest=nest, lonlat=True)
         self.ra = ra
         self.dec = dec
 
-    def get_pixel_corners(self):
-        coords = hp.boundaries(self.nside, self.pixelnum, step=1,
-                               nest=self.nest)
+    def get_pixel_corners(self, nside, nest):
+        coords = hp.boundaries(nside, self.pixelnum, step=1,
+                               nest=nest)
         ras, decs = hp.pixelfunc.vec2ang(np.transpose(coords), lonlat=True)
         self.pix_corner_ras = ras
         self.pix_corner_decs = decs
 
 
 if __name__ == '__main__':
-    plot_filled_pixels('/Users/ruby/EoR/1130776744_uniform_Residual_I_HEALPix.fits')
+    plot_healpix_tiling()
