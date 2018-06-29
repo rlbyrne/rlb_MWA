4609cb0caca870d50e8c052dcef6ba3e886d5a60
Ruby Byrne <rlbyrne@uw.edu>
2018-06-29 14:32:29 -0700

    more flexible array simulator

--- simulate_antenna_locations/array_simulate.py
+++ simulate_antenna_locations/array_simulate.py
@@ -22,8 +22,11 @@ def create_hex_array(side_length, antenna_spacing, save_uvfits=True,
         yloc = row*antenna_spacing*(3.**.5)/2
         if row % 2 == 0:
             new_xlocs = [
-                i*antenna_spacing for i in range(-side_length+row/2+1, side_length-row/2)
-                ]
+                i*antenna_spacing for i in range(
+                    -side_length+row/2+1,
+                    side_length-row/2
+                )
+            ]
             antenna_xlocs.extend(new_xlocs)
             antenna_ylocs.extend([yloc]*len(new_xlocs))
             if row != 0:
@@ -31,8 +34,11 @@ def create_hex_array(side_length, antenna_spacing, save_uvfits=True,
                 antenna_ylocs.extend([-yloc]*len(new_xlocs))
         else:
             new_xlocs = [
-                (i+.5)*antenna_spacing for i in range(int(-side_length+row/2.+.5), int(side_length-row/2.-.5))
-                ]
+                         (i+.5)*antenna_spacing for i in range(
+                            int(-side_length+row/2.+.5),
+                            int(side_length-row/2.-.5)
+                          )
+                         ]
             antenna_xlocs.extend(new_xlocs*2)
             antenna_ylocs.extend([yloc]*len(new_xlocs))
             antenna_ylocs.extend([-yloc]*len(new_xlocs))
@@ -46,39 +52,51 @@ def create_hex_array(side_length, antenna_spacing, save_uvfits=True,
         plt.xlabel('East/West Location (m)')
         plt.ylabel('North/South Location (m)')
         plt.gca().set_aspect('equal', adjustable='box')
-        plt.savefig('/Users/rubybyrne/array_simulation/antenna_hex.png')
+        plt.savefig('/Users/rubybyrne/array_simulation/'
+            'antenna_hex_{}m.png'.format(int(antenna_spacing))
+        )
         plt.close()
 
     # find the array's radial distribution
     radial_dist = [
-        (antenna_xlocs[i]**2+antenna_ylocs[i]**2)**.5 for i in range(len(antenna_xlocs))
-        ]
+        (antenna_xlocs[i]**2+antenna_ylocs[i]**2)**.5 for i in range(
+            len(antenna_xlocs)
+        )
+    ]
     radial_hist, bin_edges = np.histogram(
         radial_dist, bins=int(side_length*.75)
-        )
+    )
     bin_centers = [
         (bin_edges[i]+bin_edges[i+1])/2 for i in range(len(bin_edges)-1)
-        ]
+    ]
     plt.figure()
     plt.scatter(bin_centers, radial_hist)
-    plt.savefig('/Users/rubybyrne/array_simulation/antenna_hex_dist.png')
+    plt.savefig('/Users/rubybyrne/array_simulation/'
+        'antenna_hex_dist_{}m.png'.format(antenna_spacing)
+    )
     plt.close()
 
-    if save_uvfits:
-        create_uvfits(antennas, antenna_xlocs, antenna_ylocs, '/Users/rubybyrne/array_simulation/hex_array_sim.uvfits')
-
     print antennas
+
+    if save_uvfits:
+        create_uvfits(antennas,
+                      antenna_xlocs,
+                      antenna_ylocs,
+                      '/Users/rubybyrne/array_simulation/'
+                      'hex_array_sim_{}m.uvfits'.format(antenna_spacing)
+                      )
 
     return antennas, radial_hist, bin_centers
 
 
-def create_random_array():
+def create_random_array(antenna_spacing):
 
     save_uvfits = True
     plot_array = True
+    number_of_arrays = 5
 
-    antenna_size = 5.
-    antennas, radial_hist, bin_centers = create_hex_array(7, 15.,
+    antenna_size = antenna_spacing/3.  # Minimum antenna spacing
+    antennas, radial_hist, bin_centers = create_hex_array(7, antenna_spacing,
                                                           save_uvfits=False)
     print antennas
     radial_vals = np.arange(bin_centers[0], bin_centers[-1], .1)
@@ -86,53 +104,65 @@ def create_random_array():
     norm_factor = sum(radial_distribution)
     radial_distribution = [i/norm_factor for i in radial_distribution]
 
-    antenna_xlocs = []
-    antenna_ylocs = []
-    for ant in range(antennas):
+    for array in range(1, number_of_arrays+1):
 
-        random = np.random.uniform()
-        dist_sum = 0.
-        for i, dist_val in enumerate(radial_distribution):
-            dist_sum += dist_val
-            if random < dist_sum:
-                break
-        radius = radial_vals[i]
+        antenna_xlocs = []
+        antenna_ylocs = []
+        for ant in range(antennas):
 
-        check_overlap = False
-        while not check_overlap:
-            azimuth = np.random.uniform()*2*math.pi
-            xloc = radius*math.cos(azimuth)
-            yloc = radius*math.sin(azimuth)
+            random = np.random.uniform()
+            dist_sum = 0.
+            for i, dist_val in enumerate(radial_distribution):
+                dist_sum += dist_val
+                if random < dist_sum:
+                    break
+            radius = radial_vals[i]
 
-            # Check that the antenna does not overlap others
-            check_overlap = True
-            for i in range(len(antenna_xlocs)):
-                if abs(antenna_xlocs[i]-xloc) < antenna_size and abs(antenna_ylocs[i]-yloc) < antenna_size:
-                    check_overlap = False
-            if check_overlap:
-                antenna_xlocs.append(xloc)
-                antenna_ylocs.append(yloc)
+            check_overlap = False
+            while not check_overlap:
+                azimuth = np.random.uniform()*2*math.pi
+                xloc = radius*math.cos(azimuth)
+                yloc = radius*math.sin(azimuth)
 
-    if plot_array:
-        plt.figure()
-        plt.scatter(antenna_xlocs, antenna_ylocs, marker='o', s=1.)
-        # plt.xlim(ra_plot_range[0],ra_plot_range[1])
-        # plt.ylim(dec_plot_range[0],dec_plot_range[1])
-        plt.xlabel('East/West Location (m)')
-        plt.ylabel('North/South Location (m)')
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.savefig('/Users/rubybyrne/array_simulation/antenna_random1.png')
-        plt.close()
+                # Check that the antenna does not overlap others
+                check_overlap = True
+                for i in range(len(antenna_xlocs)):
+                    if (abs(antenna_xlocs[i]-xloc) < antenna_size
+                        and abs(antenna_ylocs[i]-yloc) < antenna_size
+                    ):
+                        check_overlap = False
+                if check_overlap:
+                    antenna_xlocs.append(xloc)
+                    antenna_ylocs.append(yloc)
 
-    if save_uvfits:
-        create_uvfits(antennas, antenna_xlocs, antenna_ylocs, '/Users/rubybyrne/array_simulation/random1_array_sim.uvfits')
+        if plot_array:
+            plt.figure()
+            plt.scatter(antenna_xlocs, antenna_ylocs, marker='o', s=1.)
+            # plt.xlim(ra_plot_range[0],ra_plot_range[1])
+            # plt.ylim(dec_plot_range[0],dec_plot_range[1])
+            plt.xlabel('East/West Location (m)')
+            plt.ylabel('North/South Location (m)')
+            plt.gca().set_aspect('equal', adjustable='box')
+            plt.savefig('/Users/rubybyrne/array_simulation/'
+                'antenna_random{}_{}m.png'.format(array, int(antenna_spacing))
+            )
+            plt.close()
+
+        if save_uvfits:
+            print 'creating uvfits'
+            create_uvfits(antennas, antenna_xlocs, antenna_ylocs,
+                '/Users/rubybyrne/array_simulation/'
+                'random{}_array_sim_{}m.uvfits'.format(array,
+                                                       int(antnna_spacing)
+                                                       )
+            )
 
 
 def create_uvfits(antennas, antenna_xlocs, antenna_ylocs, save_filename):
 
-    antenna_locs_ENU = np.zeros((antennas, 3))
-    antenna_locs_ENU[:, 0] = antenna_xlocs
-    antenna_locs_ENU[:, 1] = antenna_ylocs
+    antenna_locs_ENU = np.zeros((antennas+1, 3))  # Add an extra antenna because there must be 128 antenna locs
+    antenna_locs_ENU[:-1, 0] = antenna_xlocs
+    antenna_locs_ENU[:-1, 1] = antenna_ylocs
 
     filename = '/Users/Shared/uvfits/4.1/1061316296.uvfits'
     UV = UVData()
@@ -143,23 +173,28 @@ def create_uvfits(antennas, antenna_xlocs, antenna_ylocs, save_filename):
     UV.unphase_to_drift()  # unphase data
     ant1_list = list(set(UV.ant_1_array))
     ant2_list = list(set(UV.ant_2_array))
-    if ant1_list != antennas or ant2_list != antennas:
+    UV.select(antenna_nums=ant1_list)
+    if len(ant1_list) != antennas or len(ant2_list) != antennas:
         print 'ERROR: Incorrect number of antennas in reference uvfits'
         sys.exit(1)
-    for ant1 in ant1_list:
-        for ant2 in ant2_list:
+    for ind1, ant1 in enumerate(ant1_list):
+        for ind2, ant2 in enumerate(ant2_list):
             baseline_inds = np.intersect1d(
                 np.where(UV.ant_1_array == ant1)[0],
                 np.where(UV.ant_2_array == ant2)[0]
                 )
-            UV.uvw_array[baseline_inds, :] = antenna_locs_ENU[ant2, :] - antenna_locs_ENU[ant1, :]
+            UV.uvw_array[baseline_inds, :] = (antenna_locs_ENU[ind2, :]
+                                              - antenna_locs_ENU[ind1, :]
+                                              )
     antenna_locs_ECEF = uvutils.ECEF_from_ENU(
         antenna_locs_ENU.T, *UV.telescope_location_lat_lon_alt
         ).T
     UV.antenna_positions = antenna_locs_ECEF - UV.telescope_location
     UV.phase(phase_center_ra, phase_center_dec, phase_center_epoch)
+    print 'Saving uvfits to {}'.format(save_filename)
     UV.write_uvfits(save_filename, spoof_nonessential=True)
 
 
 if __name__ == '__main__':
-    create_hex_array(7, 15.)
+    #create_hex_array(7, 10.)
+    create_random_array(10.)
