050ef6b196d1c0b0535390ea701743456c57e89a
Ruby Byrne <rlbyrne@uw.edu>
2019-02-17 15:37:56 -0800

    refactoring healpix code for speed

--- sky_imaging/healpix_utils.py
+++ sky_imaging/healpix_utils.py
@@ -13,7 +13,7 @@ class HealpixMap:
 
     def __init__(self, signal_arr, pix_arr, nside, nest=False,
                  coords='equitorial'):
-        if pix_arr == []:
+        if len(pix_arr) == 0:
             print 'No pixel values supplied. Assuming implicit ordering.'
         else:
             if len(signal_arr) != len(pix_arr):
@@ -26,95 +26,84 @@ class HealpixMap:
         else:
             print 'ERROR: Invalid nest parameter. Exiting.'
             sys.exit(1)
-        self.signal_arr = signal_arr
-        self.pix_arr = pix_arr
+        self.signal_arr = np.array(signal_arr)
+        self.pix_arr = np.array(pix_arr)
         self.nside = nside
         self.nest = nest
         self.coords = coords
 
     def get_ra_dec(self, ra_cut=270):
-        ra_arr = []
-        dec_arr = []
-        rot = hp.rotator.Rotator(coord=['G', 'C'])
-        for pixel in self.pix_arr:
-            if self.coords == 'galactic':
-                theta_gal, phi_gal = hp.pixelfunc.pix2ang(
-                    self.nside, pixel, nest=self.nest
-                    )
-                theta_eq, phi_eq = rot(theta_gal, phi_gal)
-                ra = phi_eq*180/math.pi
-                dec = 90.-theta_eq*180/math.pi
-            elif self.coords == 'equitorial':
-                ra, dec = hp.pixelfunc.pix2ang(
-                    self.nside, pixel, nest=self.nest, lonlat=True
-                    )
-            else:
-                print 'ERROR: Coordinates must be galactic or equitorial.'
-                sys.exit(1)
-            if ra > ra_cut:
-                ra -= 360.
-            elif ra < ra_cut-360.:
-                ra += 360.
-            ra_arr.append(ra)
-            dec_arr.append(dec)
+        if self.coords == 'galactic':
+            theta_gal, phi_gal = hp.pixelfunc.pix2ang(
+                self.nside, self.pix_arr, nest=self.nest
+                )
+            rot = hp.rotator.Rotator(coord=['G', 'C'])
+            theta_eq, phi_eq = rot(theta_gal, phi_gal)
+            ra_arr = phi_eq*180/math.pi
+            dec_arr = 90.-theta_eq*180/math.pi
+        elif self.coords == 'equitorial':
+            ra_arr, dec_arr = hp.pixelfunc.pix2ang(
+                self.nside, self.pix_arr, nest=self.nest, lonlat=True
+                )
+        else:
+            print 'ERROR: Coordinates must be galactic or equitorial.'
+            sys.exit(1)
+        ra_arr[np.where(ra_arr > ra_cut)] = ra_arr[np.where(ra_arr > ra_cut)] - 360.
+        ra_arr[np.where(ra_arr < ra_cut-360.)] = ra_arr[np.where(ra_arr > ra_cut)] + 360.
         self.ra_arr = ra_arr
         self.dec_arr = dec_arr
 
     def get_pixel_corners(self, ra_cut=270):
-        pix_corner_ras_arr = []
-        pix_corner_decs_arr = []
-        rot = hp.rotator.Rotator(coord=['G', 'C'])
-        for pixel in self.pix_arr:
-            corner_coords = hp.boundaries(self.nside, pixel, step=1,
-                                          nest=self.nest)
-            if self.coords == 'galactic':
-                thetas_gal, phis_gal = hp.pixelfunc.vec2ang(
-                    np.transpose(corner_coords), lonlat=False)
-                ras = [0]*len(thetas_gal)
-                decs = [0]*len(thetas_gal)
-                for corner in range(len(thetas_gal)):
-                    theta_eq, phi_eq = rot(thetas_gal[corner],
-                                           phis_gal[corner])
-                    ras[corner] = phi_eq*180/math.pi
-                    decs[corner] = 90. - theta_eq*180/math.pi
-            elif self.coords == 'equitorial':
-                ras, decs = hp.pixelfunc.vec2ang(np.transpose(corner_coords),
-                                                 lonlat=True)
-            else:
-                print 'ERROR: Coordinates must be galactic or equitorial.'
-                sys.exit(1)
-            # Correct for the branch cut
-            for ind, ra_val in enumerate(ras):
-                if ra_val > ra_cut:
-                    ras[ind] = ra_val-360.
-                if ra_val < ra_cut-360.:
-                    ras[ind] = ra_val+360.
-                # Cluster pixel corners that are separated by the branch cut
-                if ind > 0:
-                    if abs(ras[ind]-ras[0]-360.) < abs(ras[ind]-ras[0]):
-                        ras[ind] -= 360.
-                    elif abs(ras[ind]-ras[0]+360.) < abs(ras[ind]-ras[0]):
-                        ras[ind] += 360.
-            pix_corner_ras_arr.append(ras)
-            pix_corner_decs_arr.append(decs)
-        self.pix_corner_ras_arr = pix_corner_ras_arr
-        self.pix_corner_decs_arr = pix_corner_decs_arr
+        corner_coords = hp.boundaries(
+            self.nside, self.pix_arr, step=1, nest=self.nest
+        )
+        if self.coords == 'galactic':
+            thetas_gal = []
+            phis_gal = []
+            for pixel in range(len(self.signal_arr)):
+                thetas_gal_pix, phis_gal_pix = hp.pixelfunc.vec2ang(
+                    np.transpose(corner_coords[pixel]), lonlat=False
+                )
+                thetas_gal.append(thetas_gal_pix)
+                phis_gal.append(phis_gal_pix)
+            thetas_gal = np.array(thetas_gal)
+            phis_gal = np.array(phis_gal)
+            rot = hp.rotator.Rotator(coord=['G', 'C'])
+            thetas_eq, phis_eq = rot(thethas_gal, phis_gal)
+            ras = phis_eq*180/math.pi
+            decs = 90. - thetas_eq*180/math.pi
+        elif self.coords == 'equitorial':
+            ras = []
+            decs = []
+            for pixel in range(len(self.signal_arr)):
+                ras_pix, decs_pix = hp.pixelfunc.vec2ang(
+                    np.transpose(corner_coords[pixel]), lonlat=True
+                )
+                ras.append(ras_pix)
+                decs.append(decs_pix)
+            ras = np.array(ras)
+            decs = np.array(decs)
+        else:
+            print 'ERROR: Coordinates must be galactic or equitorial.'
+            sys.exit(1)
+        ras[np.where(ras > ra_cut)] = ras[np.where(ras > ra_cut)] - 360.
+        ras[np.where(ras < ra_cut-360.)] = ras[np.where(ras > ra_cut)] + 360.
+        self.pix_corner_ras_arr = ras
+        self.pix_corner_decs_arr = decs
 
     def explicit_to_implicit_ordering(self):
-        signal_vals_implicit = [hp.pixelfunc.UNSEEN]*12*self.nside**2
-        for i in range(len(self.signal_arr)):
-            signal_vals_implicit[self.pix_arr[i]] = self.signal_arr[i]
+        signal_vals_implicit = np.full(12*self.nside**2, hp.pixelfunc.UNSEEN)
+        signal_vals_implicit[self.pix_arr] = self.signal_arr
         self.signal_arr = signal_vals_implicit
-        self.pix_arr = []
+        self.pix_arr = np.empty(0)
 
     def implicit_to_explicit_ordering(self):
-        use_inds = [ind for ind in range(len(self.signal_arr))
-                    if self.signal_arr[ind] != hp.pixelfunc.UNSEEN]
-        if self.pix_arr == []:
+        use_inds = np.where(self.signal_arr != hp.pixelfunc.UNSEEN)[0]
+        if len(self.pix_arr) == 0:
             self.pix_arr = use_inds
         else:
-            self.pix_arr = [self.pix_arr[ind] for ind in use_inds]
-        self.signal_arr = [self.signal_arr[ind] for ind in use_inds]
+            self.pix_arr = self.pix_arr[use_inds]
+        self.signal_arr = self.signal_arr[use_inds]
 
     def reorder_nest_to_ring(self):
         if self.nest:
@@ -534,7 +523,7 @@ def combine_maps_nearest_data_memory_efficient(
         data_files = os.listdir('{}/output_data/'.format(fhd_run_path))
         data_files = [
             file for file in data_files
-            if '_uniform_{}_HEALPix.fits'.format(cube_name) in file
+            if '_uniform_{}_HEALPix.fits'.format(cube_names[0]) in file
         ]
         obs_list = [file[0:10] for file in data_files]
     else:  # use the obs file list
@@ -543,6 +532,7 @@ def combine_maps_nearest_data_memory_efficient(
         obs_list = [obs.strip() for obs in obs_list]
         # remove duplicates
         obs_list = list(set(obs_list))
+    obs_list = obs_list[0:2]
 
     print 'Combining {} observations'.format(len(obs_list))
 
@@ -573,7 +563,7 @@ def combine_maps_nearest_data_memory_efficient(
 
         if obs_ind == 0:
             pix_array = obs_maps[0].pix_arr
-            signal_array = [[]*len(cube_names)]
+            signal_array = [[]]*len(cube_names)
             signal_array[0] = obs_maps[0].signal_arr
             for cube_ind in range(1, len(cube_names)):
                 signal_array[cube_ind].extend([
@@ -589,7 +579,7 @@ def combine_maps_nearest_data_memory_efficient(
                 pix for pix in obs_maps[0].pix_arr if pix in pix_array
             ]
             # Add pixels that aren't already represented in the map
-            pix_arr.extend(add_pixels)
+            pix_array.extend(add_pixels)
             for cube_ind in range(len(cube_names)):
                 signal_array[cube_ind].extend([
                     obs_maps[cube_ind].signal_arr[
@@ -616,6 +606,9 @@ def combine_maps_nearest_data_memory_efficient(
 
     combined_maps = []
     for cube_ind in range(len(cube_names)):
+        print len(signal_array[cube_ind])
+        print len(pix_array)
+        print cube_ind
         map = HealpixMap(
             signal_array[cube_ind], pix_array, nside, nest=nest, coords=coords
         )
