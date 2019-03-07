6d78e8aa6bafcc9289a78d6050bbf56ffc0b0df5
Ruby Byrne <rlbyrne@uw.edu>
2019-03-07 02:05:55 -0800

    triangle plotting is working

--- deconvolution_qa/qa_utils.py
+++ deconvolution_qa/qa_utils.py
@@ -50,7 +50,9 @@ def write_params_to_csv(
     )
 
 
-def qa_param_histogram(filepath, param_name, saveloc, logx=False, logy=False):
+def qa_param_histogram(
+    filepath, param_name, saveloc='', logx=False, logy=False
+):
 
     # Get header
     file = open(filepath, 'r')
@@ -61,19 +63,12 @@ def qa_param_histogram(filepath, param_name, saveloc, logx=False, logy=False):
     use_params = params[:, header.index(param_name)]
     use_params = use_params[np.where(~np.isnan(use_params))[0]]
 
+    # Histogram data
     if logx:
         hist, bin_edges = np.histogram(np.log(use_params), bins='auto')
         bin_edges = np.exp(bin_edges)
     else:
         hist, bin_edges = np.histogram(use_params, bins='auto')
-    # histogram data
-    ref_hist, bin_edges = np.histogram(
-        [np.log(val) for val in ref_catalog_fluxes], bins=bin_edges
-    )
-    decon_hist, bin_edges = np.histogram(
-        [np.log(val) for val in decon_catalog_fluxes], bins=bin_edges
-    )
-    bin_edges = [np.exp(val) for val in bin_edges]
     xvals = [bin_edges[0]]
     yvals = [0.]
     for i in range(len(bin_edges)-1):
@@ -87,14 +82,18 @@ def qa_param_histogram(filepath, param_name, saveloc, logx=False, logy=False):
         plt.xscale('log')
     plt.plot(xvals, yvals, linestyle='-', color='blue', linewidth=0.5,
              label='GLEAM flux densities')
-    plt.fill_between(xvals, 0, yvals, facecolor='blue', alpha=0.1)
     if logy:
         plt.yscale('log')
+    else:  # Fill gets weird with a log y-axis
+        plt.fill_between(xvals, 0, yvals, facecolor='blue', alpha=0.1)
     plt.xlabel(param_name)
     plt.ylabel('Histogram Count')
-    print 'Saving plot to {}'.format(saveloc)
-    plt.savefig(saveloc, dpi=300)
-    plt.close()
+    if saveloc == '':
+        plt.show()
+    else:
+        print 'Saving plot to {}'.format(saveloc)
+        plt.savefig(saveloc, dpi=300)
+        plt.close()
 
 
 def qa_param_hist2d(
@@ -115,7 +114,9 @@ def qa_param_hist2d(
     use_params_y = use_params_y[use_indices]
 
     # Histogram data
-    hist, x_edges, y_edges = np.histogram2d(use_params_x, use_params_y, bins=10)
+    hist, x_edges, y_edges = np.histogram2d(
+        use_params_x, use_params_y, bins=10
+    )
     if colorbar_range is None:
         colorbar_range = [np.min(hist), np.max(hist)]
 
@@ -123,7 +124,7 @@ def qa_param_hist2d(
     plt.imshow(
         hist, interpolation='none', origin='low',
         extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]],
-        vmin=colorbar_range[0], vmax=colorbar_range[1]
+        vmin=colorbar_range[0], vmax=colorbar_range[1], aspect='auto'
     )
     plt.xlabel(x_param_name)
     plt.ylabel(y_param_name)
@@ -145,8 +146,9 @@ def qa_param_hist_set(filepath, param_names, saveloc='', colorbar_range=None):
 
     params = np.genfromtxt(filepath, delimiter=',', skip_header=1)
 
+    fig = plt.figure(figsize=(6, 6))
     grid = plt.GridSpec(
-        2*len(param_names)-1, 2*len(param_names)-1, wspace=0.4, hspace=0.3
+        2*len(param_names)-1, 2*len(param_names)-1, wspace=0.1, hspace=0.1
     )
 
     for ind1, param1 in enumerate(param_names[:-1]):
@@ -155,35 +157,66 @@ def qa_param_hist_set(filepath, param_names, saveloc='', colorbar_range=None):
             use_params_y = params[:, header.index(param2)]
             xrange = [np.min(use_params_x), np.max(use_params_x)]
             yrange = [np.min(use_params_y), np.max(use_params_y)]
-            fig = plt.subplot(
-                grid[2*ind1+1:2*ind1+3, 2*(ind2+ind1):2*(ind2+ind1)+2]
+            print grid[2*ind1+1:2*ind1+3, 2*(ind2+ind1):2*(ind2+ind1)+2]
+            subfig = fig.add_subplot(
+                grid[2*(ind2+ind1):2*(ind2+ind1)+2, 2*ind1+1:2*ind1+3]
             )
-            fig.plot(use_params_x, use_params_y, 'ok', markersize=3, alpha=0.2)
-            hist, x_edges, y_edges = np.histogram2d(
-                use_params_x, use_params_y, bins=10
-            )
-            x_centers = [
-                (x_edges[i]+x_edges[i+1])/2 for i in range(len(x_edges)-1)
-            ]
-            y_centers = [
-                (y_edges[i]+y_edges[i+1])/2 for i in range(len(y_edges)-1)
-            ]
-            fig.contour(x_centers, y_centers, hist)
+            subfig.plot(use_params_x, use_params_y, 'ok', markersize=3, alpha=0.2)
+            subfig.axes.get_xaxis().set_visible(False)
+            subfig.axes.get_yaxis().set_visible(False)
+
+            if False:
+                hist, x_edges, y_edges = np.histogram2d(
+                    use_params_x, use_params_y, bins=100
+                )
+                x_centers = [
+                    (x_edges[i]+x_edges[i+1])/2 for i in range(len(x_edges)-1)
+                ]
+                y_centers = [
+                    (y_edges[i]+y_edges[i+1])/2 for i in range(len(y_edges)-1)
+                ]
+                subfig.contour(
+                    x_centers, y_centers, hist.T, levels=[np.max(hist)/10000., np.max(hist)/100.], colors=['skyblue', 'dodgerblue'],
+                    origin='image'
+                )
 
             if ind2 == 0:
-                x_hist = plt.subplot(grid[2*ind1+1:2*ind1+3, -1:])
+                x_hist = fig.add_subplot(grid[-1, 2*ind1+1:2*ind1+3])
                 x_hist.hist(
-                    use_params_x, histtype='stepfilled', orientation='vertical',
-                    color='gray', range=xrange
+                    use_params_x,
+                    orientation='vertical', color='gray', density=True
                 )
                 x_hist.invert_yaxis()
-                x_hist.xlabel(param1)
+                x_hist.get_shared_x_axes().join(x_hist, subfig)
+                x_hist.set_xlabel(param1)
+                x_hist.axes.get_yaxis().set_visible(False)
+            else:
+                subfig.get_shared_x_axes().join(x_hist, subfig)
 
             if ind1 == 0:
-                y_hist = plt.subplot(grid[:2, 2*(ind2+ind1):2*(ind2+ind1)+2])
+                y_hist = fig.add_subplot(
+                    grid[2*(ind2+ind1):2*(ind2+ind1)+2, 0], sharey=subfig
+                )
                 y_hist.hist(
-                    use_params_y, histtype='stepfilled', orientation='horizontal',
-                    color='gray', range=yrange
+                    use_params_y,
+                    orientation='horizontal', color='gray', density=True
                 )
                 y_hist.invert_xaxis()
-                y_hist.ylabel(param2)
+                y_hist.get_shared_y_axes().join(y_hist, subfig)
+                y_hist.set_ylabel(param2)
+                y_hist.axes.get_xaxis().set_visible(False)
+            else:
+                subfig.get_shared_y_axes().join(y_hist, subfig)
+
+
+    if saveloc == '':
+        plt.show()
+    else:
+        print 'Saving plot to {}'.format(saveloc)
+        plt.savefig(saveloc, dpi=300)
+        plt.close()
+
+
+if __name__ == '__main__':
+
+    qa_param_hist_set('/Users/ruby/EoR/diffuse_total_occ.csv', ['obsid', 'RFI occupancy','obsid', 'RFI occupancy'], colorbar_range=None)
