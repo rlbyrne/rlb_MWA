7143667cc4de4240a7203b4400a9b05849a73148
Ruby Byrne <rlbyrne@uw.edu>
2019-09-20 13:51:44 -0700

    updated calibration calculation

--- delay_calibration/fft_testing.py
+++ delay_calibration/fft_testing.py
@@ -20,57 +20,77 @@ plt.plot(output)
 plt.plot(np.abs(return_input))
 plt.show()
 """
-"""
-metadata = UVData()
-metadata.read_uvfits('/Users/ruby/EoR/1061316296.uvfits', read_data=False)
-data = UVData()
-data.read_uvfits(
-    '/Users/ruby/EoR/1061316296.uvfits', times=metadata.time_array[200000],
-    polarizations=0  # supports only one polarization
-)
 
-# Reformat data array:
-data_vis_array = np.full(
-    (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
-    np.nan, dtype=complex
-)
-for bl_ind in range(data.Nbls):
-    data_vis_array[
-        data.ant_1_array[bl_ind], data.ant_2_array[bl_ind], :, :
-    ] = data.data_array[bl_ind, 0, :, :]
-    data_vis_array[
-        data.ant_2_array[bl_ind], data.ant_1_array[bl_ind], :, :
-    ] = np.conj(data.data_array[bl_ind, 0, :, :])
-# Reformat model array:
-model_vis_array = np.full(
-    (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
-    np.nan, dtype=complex
-)
-for bl_ind in range(model.Nbls):
-    model_vis_array[
-        model.ant_1_array[bl_ind], model.ant_2_array[bl_ind], :, :
-    ] = model.data_array[bl_ind, 0, :, :]
-    model_vis_array[
-        model.ant_2_array[bl_ind], model.ant_1_array[bl_ind], :, :
-    ] = np.conj(model.data_array[bl_ind, 0, :, :])
-# Initialize weights:
-vis_weights_array = np.full(
-    (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
-    1., dtype=float
-)
-vis_weights_array[np.where(np.isnan(data_vis_array))[[0, 1, 2, 4]]] = 0.
-vis_weights_array[np.where(np.isnan(model_vis_array))[[0, 1, 2, 4]]] = 0.
-"""
+
+def calibrate_test_data():
+
+    metadata = UVData()
+    metadata.read_uvfits('/Users/ruby/EoR/1061316296.uvfits', read_data=False)
+    data = UVData()
+    data.read_uvfits(
+        '/Users/ruby/EoR/1061316296.uvfits', times=metadata.time_array[200000],
+        polarizations=-5  # supports only one polarization: -5=XX
+    )
+    # Data has shape (Nblts, Nspws, Nfreqs, Npols)
+    data_times = np.unique(data.time_array)
+    data_antennas = np.unique(np.append(data.ant_1_array, data.ant_2_array))
+
+    # Reformat data array:
+    data_vis_array = np.full(
+        (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
+        np.nan, dtype=complex
+    )
+    for ind in range(data.Nblts):
+        ant1 = np.where((data_antennas == data.ant_1_array[ind]))[0][0]
+        ant2 = np.where((data_antennas == data.ant_2_array[ind]))[0][0]
+        time = np.where((data_times == data.time_array[ind]))[0][0]
+        data_vis_array[ant1, ant2, time, :] = data.data_array[ind, 0, :, 0]
+        data_vis_array[ant2, ant1, time, :] = np.conj(
+            data.data_array[ind, 0, :, 0]
+        )
+
+    # Use dummy data for the model:
+    model = data
+    model.data_array *= 1.2
+
+    # Reformat model array:
+    model_vis_array = np.full(
+        (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
+        np.nan, dtype=complex
+    )
+    for ind in range(data.Nblts):
+        ant1 = np.where((data_antennas == model.ant_1_array[ind]))[0][0]
+        ant2 = np.where((data_antennas == model.ant_2_array[ind]))[0][0]
+        time = np.where((data_times == model.time_array[ind]))[0][0]
+        model_vis_array[ant1, ant2, time, :] = model.data_array[ind, 0, :, 0]
+        model_vis_array[ant2, ant1, time, :] = np.conj(
+            model.data_array[ind, 0, :, 0]
+        )
+
+    # Initialize weights:
+    vis_weights_array = np.full(
+        (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
+        1., dtype=float
+    )
+    data_nans = np.where(np.isnan(data_vis_array))
+    if len(data_nans[0]) > 0:
+        vis_weights_array[data_nans[[0, 1, 2, 4]]] = 0.
+    model_nans = np.where(np.isnan(model_vis_array))
+    if len(model_nans[0]) > 0:
+        vis_weights_array[model_nans[[0, 1, 2, 4]]] = 0.
+
+    calibration_routine(data_vis_array, model_vis_array, vis_weights_array)
+
 
 def calibration_routine(
     data_vis_array, model_vis_array, vis_weights_array, delay_calibrate=False,
-    convergence_threshold=.2, max_iter=100, gain_factor=.1, use_autos=False
+    convergence_threshold=.01, max_iter=100, gain_factor=.1, use_autos=False
 ):
 
     Nants, Ntimes, Nfreqs = data_vis_array.shape[1:4]
 
     # Initialize gains to 1
-    gains = np.full((Nants, Nfreqs), 1.)
+    gains = np.full((Nants, Nfreqs), 1., dtype=complex)
 
     if not use_autos:
         for ind in range(Nants):
@@ -106,7 +126,6 @@ def calibration_routine(
                     * np.conj(gains_ant2_mat)/np.abs(gains_ant2_mat)
                 ), axis=(0, 2, 3)
             )
-            print gain_amp_grad[0]
             # Calculate the gain phase gradient:
             gain_phase_grad = 4*np.sum(
                 vis_weights_array*np.imag(data_vis_array[:, :, :, mode])
@@ -133,13 +152,20 @@ def calibration_routine(
                     * model_vis_array[:, :, :, mode]
                 ), axis1=0, axis2=1
             ))
-            new_gains_amp = (
-                np.abs(gains[:, mode])-chi_squared/gain_amp_grad*gain_factor
-            )
-            new_gains_phase = (
-                np.angle(gains[:, mode])
-                - chi_squared/gain_phase_grad*gain_factor
-            )
+            print np.max(np.imag(data_vis_array[:, :, :, mode]))
+            print gain_phase_grad
+
+            new_gains_amp = np.abs(gains[:, mode])
+            new_gains_phase = np.angle(gains[:, mode])
+            for ant_i in range(Nants):
+                if gain_amp_grad[ant_i] != 0:
+                    new_gains_amp[ant_i] -= (
+                        chi_squared / gain_amp_grad[ant_i] * gain_factor
+                    )
+                if gain_phase_grad[ant_i] != 0:
+                    new_gains_phase[ant_i] -= (
+                        chi_squared / gain_phase_grad[ant_i] * gain_factor
+                    )
             gains[:, mode] = new_gains_amp * np.exp(
                 np.complex(0, 1) * new_gains_phase
             )
@@ -148,33 +174,32 @@ def calibration_routine(
                 )[:, :, np.newaxis], Ntimes, axis=2
             )
             gains_ant2_mat = np.transpose(gains_ant1_mat, axes=[1, 0, 2])
-            break
-            """
             new_chi_squared = np.sum(vis_weights_array * np.abs(
-                data_vis_array[:, :, :, mode, :]
+                data_vis_array[:, :, :, mode]
                 - gains_ant1_mat * np.conj(gains_ant2_mat)
-                * model_vis_array[:, :, :, mode, :]
+                * model_vis_array[:, :, :, mode]
             )**2.)
-            convergence = (new_chi_squared-chi_squared)/chi_squared
+            convergence = np.abs(new_chi_squared-chi_squared)
+            print chi_squared
             chi_squared = new_chi_squared
             iter += 1
-            """
 
 
 if __name__ == '__main__':
-    data_vis_array = np.full(
-        (5, 5, 1, 3),
+    """data_vis_array = np.full(
+        (5, 5, 1, 1),
         1., dtype=complex
     )
     model_vis_array = np.full(
-        (5, 5, 1, 3),
+        (5, 5, 1, 1),
         1.1, dtype=complex
     )
     vis_weights_array = np.full(
-        (5, 5, 1, 3),
+        (5, 5, 1, 1),
         1., dtype=float
     )
     calibration_routine(
         data_vis_array, model_vis_array, vis_weights_array, delay_calibrate=False,
-        convergence_threshold=.2, max_iter=100, gain_factor=.1, use_autos=False
-    )
+        max_iter=100, use_autos=False
+    )"""
+    calibrate_test_data()
