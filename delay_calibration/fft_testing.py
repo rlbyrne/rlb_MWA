4089d46fe5132d289b5763f370d24e6d64630c32
Ruby Byrne <rlbyrne@uw.edu>
2019-09-23 15:29:06 -0700

    first functioning gradient descent algorithm

--- delay_calibration/fft_testing.py
+++ delay_calibration/fft_testing.py
@@ -35,7 +35,7 @@ def calibrate_test_data():
     data_times = np.unique(data.time_array)
     data_antennas = np.unique(np.append(data.ant_1_array, data.ant_2_array))
 
-    # Reformat data array:
+    # Reformat data array to have shape (Nants, Nants, Ntimes, Nfreqs)
     data_vis_array = np.full(
         (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
         np.nan, dtype=complex
@@ -51,9 +51,8 @@ def calibrate_test_data():
 
     # Use dummy data for the model:
     model = data
-    model.data_array *= 1.2
 
-    # Reformat model array:
+    # Reformat model array to have shape (Nants, Nants, Ntimes, Nfreqs)
     model_vis_array = np.full(
         (data.Nants_data, data.Nants_data, data.Ntimes, data.Nfreqs),
         np.nan, dtype=complex
@@ -66,6 +65,10 @@ def calibrate_test_data():
         model_vis_array[ant2, ant1, time, :] = np.conj(
             model.data_array[ind, 0, :, 0]
         )
+
+    model_vis_array *= (1.2+0.001*1j)  # make model different from data
+    model_vis_array[0, 1, :, :] = 22.
+    model_vis_array[1, 0, :, :] = 22.
 
     # Initialize weights:
     vis_weights_array = np.full(
@@ -84,17 +87,18 @@ def calibrate_test_data():
 
 def calibration_routine(
     data_vis_array, model_vis_array, vis_weights_array, delay_calibrate=False,
-    convergence_threshold=.01, max_iter=100, gain_factor=.1, use_autos=False
+    convergence_threshold=1, max_iter=10000, gain_factor=1e-3, use_autos=False
 ):
 
+    floating_pt_error_threshold = 1e-7
     Nants, Ntimes, Nfreqs = data_vis_array.shape[1:4]
 
     # Initialize gains to 1
     gains = np.full((Nants, Nfreqs), 1., dtype=complex)
 
-    if not use_autos:
+    if not use_autos:  # autocalibration is currently not supported
         for ind in range(Nants):
-            vis_weights_array[ind, ind, :, :] = 0.
+            vis_weights_array[ind, ind, :] = 0.
 
     for mode in range(Nfreqs):
         gains_ant1_mat = np.repeat(np.repeat(
@@ -102,7 +106,7 @@ def calibration_routine(
             )[:, :, np.newaxis], Ntimes, axis=2
         )
         gains_ant2_mat = np.transpose(gains_ant1_mat, axes=[1, 0, 2])
-        chi_squared = np.sum(vis_weights_array * np.abs(
+        chi_squared = np.sum(vis_weights_array[:, :, :, mode] * np.abs(
             data_vis_array[:, :, :, mode]
             - gains_ant1_mat * np.conj(gains_ant2_mat)
             * model_vis_array[:, :, :, mode]
@@ -113,89 +117,80 @@ def calibration_routine(
         iter = 0  # initialize iter parameter
         while iter <= max_iter and convergence >= convergence_threshold:
 
-            # Calculate the gain amplitude gradient:
-            gain_amp_grad = 4*np.sum(
-                vis_weights_array*np.abs(gains_ant1_mat)**2.
+            # Calculate the gradient of the real part of the gains
+            gain_grad_real = 4*np.sum(vis_weights_array[:, :, :, mode]*(
+                np.real(gains_ant1_mat)
+                * np.abs(gains_ant2_mat)**2.
                 * np.abs(model_vis_array[:, :, :, mode])**2.
-                * np.abs(gains_ant2_mat),
-                axis=(0, 2, 3)
-            ) - 4*np.sum(
-                vis_weights_array*np.real(
-                    gains_ant1_mat * model_vis_array[:, :, :, mode]
-                    * np.conj(data_vis_array[:, :, :, mode])
-                    * np.conj(gains_ant2_mat)/np.abs(gains_ant2_mat)
-                ), axis=(0, 2, 3)
+                - np.real(
+                    np.conj(data_vis_array[:, :, :, mode])
+                    * np.conj(gains_ant2_mat)
+                    * model_vis_array[:, :, :, mode]
+                )
+                ), axis=(1, 2)
             )
-            # Calculate the gain phase gradient:
-            gain_phase_grad = 4*np.sum(
-                vis_weights_array*np.imag(data_vis_array[:, :, :, mode])
-                * np.real(
-                    gains_ant1_mat*np.conj(gains_ant2_mat)
-                    * model_vis_array[:, :, :, mode]
-                ), axis=(0, 2, 3)
-            ) - 4*np.sum(
-                vis_weights_array*np.real(data_vis_array[:, :, :, mode])
-                * np.imag(
-                    gains_ant1_mat*np.conj(gains_ant2_mat)
-                    * model_vis_array[:, :, :, mode]
-                ), axis=(0, 2, 3)
-            ) - 4*np.sum(np.diagonal(
-                vis_weights_array*np.imag(data_vis_array[:, :, :, mode])
-                * np.real(
-                    gains_ant1_mat*np.conj(gains_ant2_mat)
-                    * model_vis_array[:, :, :, mode]
-                ), axis1=0, axis2=1
-            )) + 4*np.sum(np.diagonal(
-                vis_weights_array*np.real(data_vis_array[:, :, :, mode])
-                * np.imag(
-                    gains_ant1_mat*np.conj(gains_ant2_mat)
-                    * model_vis_array[:, :, :, mode]
-                ), axis1=0, axis2=1
-            ))
-            print np.max(np.imag(data_vis_array[:, :, :, mode]))
-            print gain_phase_grad
 
-            new_gains_amp = np.abs(gains[:, mode])
-            new_gains_phase = np.angle(gains[:, mode])
-            for ant_i in range(Nants):
-                if gain_amp_grad[ant_i] != 0:
-                    new_gains_amp[ant_i] -= (
-                        chi_squared / gain_amp_grad[ant_i] * gain_factor
-                    )
-                if gain_phase_grad[ant_i] != 0:
-                    new_gains_phase[ant_i] -= (
-                        chi_squared / gain_phase_grad[ant_i] * gain_factor
-                    )
-            gains[:, mode] = new_gains_amp * np.exp(
-                np.complex(0, 1) * new_gains_phase
+            # Calculate the gradient of the imaginary part of the gains
+            gain_grad_imag = 4*np.sum(vis_weights_array[:, :, :, mode]*(
+                np.imag(gains_ant1_mat)
+                * np.abs(gains_ant2_mat)**2.
+                * np.abs(model_vis_array[:, :, :, mode])**2.
+                + np.imag(
+                    np.conj(data_vis_array[:, :, :, mode])
+                    * np.conj(gains_ant2_mat)
+                    * model_vis_array[:, :, :, mode]
+                )
+                ), axis=(1, 2)
             )
+
+            convergence = np.sqrt(
+                np.sum(gain_grad_real**2.) + np.sum(gain_grad_imag**2.)
+            )
+
+            new_gains_real = np.real(gains[:, mode])
+            new_gains_imag = np.imag(gains[:, mode])
+            for ant_i in range(Nants):
+                if abs(gain_grad_real[ant_i]) > floating_pt_error_threshold:
+                    new_gains_real[ant_i] -= (
+                        gain_grad_real[ant_i] / convergence * gain_factor
+                    )
+                if abs(gain_grad_imag[ant_i]) > floating_pt_error_threshold:
+                    new_gains_imag[ant_i] -= (
+                        gain_grad_imag[ant_i] / convergence * gain_factor
+                    )
+            gains[:, mode] = new_gains_real + 1j * new_gains_imag
             gains_ant1_mat = np.repeat(np.repeat(
                 gains[:, mode, np.newaxis], Nants, axis=1
                 )[:, :, np.newaxis], Ntimes, axis=2
             )
             gains_ant2_mat = np.transpose(gains_ant1_mat, axes=[1, 0, 2])
-            new_chi_squared = np.sum(vis_weights_array * np.abs(
+            new_chi_squared = np.sum(vis_weights_array[:, :, :, mode] * np.abs(
                 data_vis_array[:, :, :, mode]
                 - gains_ant1_mat * np.conj(gains_ant2_mat)
                 * model_vis_array[:, :, :, mode]
             )**2.)
-            convergence = np.abs(new_chi_squared-chi_squared)
-            print chi_squared
+            print new_chi_squared
             chi_squared = new_chi_squared
             iter += 1
+        print gains[:,0]
+        sys.exit()
 
 
 if __name__ == '__main__':
     """data_vis_array = np.full(
-        (5, 5, 1, 1),
+        (2, 2, 1, 1),
         1., dtype=complex
     )
+    data_vis_array[0, 1, 0, 0] = 1+1j
+    data_vis_array[1, 0, 0, 0] = 1-1j
     model_vis_array = np.full(
-        (5, 5, 1, 1),
-        1.1, dtype=complex
+        (2, 2, 1, 1),
+        1.2, dtype=complex
     )
+    model_vis_array[0, 1, 0, 0] = 1.1+1j
+    model_vis_array[1, 0, 0, 0] = 1.1-1j
     vis_weights_array = np.full(
-        (5, 5, 1, 1),
+        (2, 2, 1, 1),
         1., dtype=float
     )
     calibration_routine(
