6827af7b15dccdfca91c9cbebd81126e5102d5ad
Ruby Byrne <rlbyrne@uw.edu>
2020-12-01 17:24:38 -0800

    updates to unified calibration code

--- delay_calibration/unified_calibration.py
+++ delay_calibration/unified_calibration.py
@@ -473,7 +473,7 @@ def optimize_sky_cal(
         np.mean(np.cos(np.angle(gains_fit)))
     )
     gains_fit *= np.cos(avg_angle) - 1j*np.sin(avg_angle)
-    vis_fit = None
+    vis_fit = model_visibilities
 
     return gains_fit, vis_fit
 
@@ -588,6 +588,7 @@ def plot_model_errors(
         plt.show()
     else:
         plt.savefig(savepath, dpi=600)
+    plt.close()
 
 
 def produce_kde(data, xvals, yvals):
@@ -618,35 +619,41 @@ def produce_kde(data, xvals, yvals):
 
 def histogram_plot_2d(
     vals, gains=True, plot_range=None, nbins=50, colorbar_range=None,
-    plot_contours=True, savepath=None
+    plot_contours=True, savepath=None, title=None
 ):
+
+    matplotlib.rcParams.update({'font.size': 20})
 
     # Set defaults
     if gains:
         if plot_range is None:
-            plot_range = .05
+            plot_range = .03
         if colorbar_range is None:
-            colorbar_range = [0, .008]
-        axis_label = 'Frac. Gain Error'
+            colorbar_range = [0, .005]
+        axis_label = 'Fit Gain'
     else:
         if plot_range is None:
-            plot_range = .9
+            plot_range = .3
         if colorbar_range is None:
-            colorbar_range = [0, .022]
+            colorbar_range = [0, .005]
         axis_label = 'Fit Vis. Error (Jy)'
 
-    bins = np.linspace(-plot_range, plot_range, num=nbins+1)
+    bins_imag = np.linspace(-plot_range, plot_range, num=nbins+1)
+    if gains:
+        bins_real = bins_imag+1
+    else:
+        bins_real = bins_imag
 
     for data_set_ind in range(np.shape(vals)[2]):
 
         plot_data = vals[:, :, data_set_ind]
         hist, x_edges, y_edges = np.histogram2d(
             np.real(plot_data).flatten(), np.imag(plot_data).flatten(),
-            bins=bins
+            bins=[bins_real, bins_imag]
         )
         hist /= np.sum(hist)
         if plot_contours:
-            kde, percent_plot = produce_kde(plot_data, bins, bins)
+            kde, percent_plot = produce_kde(plot_data, bins_real, bins_imag)
 
         plt.figure(figsize=[7, 5.5])
         plt.imshow(
@@ -659,51 +666,47 @@ def histogram_plot_2d(
         cbar.ax.set_ylabel('Histogram Density', rotation=270, labelpad=20)
         if plot_contours:
             plt.contour(
-                bins, bins, percent_plot, levels=[.5, .9], colors='white',
-                linestyles=['solid', 'dashed'], linewidths=1
+                bins_real, bins_imag, percent_plot, levels=[.5, .9],
+                colors='white', linestyles=['solid', 'dashed'], linewidths=1
             )
-        plt.scatter(
-            [0], [0], marker='P', color='white', edgecolors='black', s=150
-        )
+        if gains:
+            plt.scatter(
+                [1], [0], marker='P', color='white', edgecolors='black', s=150
+            )
+        else:
+            plt.scatter(
+                [0], [0], marker='P', color='white', edgecolors='black', s=150
+            )
         plt.xlabel('{}, Real Part'.format(axis_label))
+        #plt.xticks(rotation=45)
         plt.ylabel('{}, Imag. Part'.format(axis_label))
+        if title is not None:
+            plt.title(title)
         plt.tight_layout()  # Ensure that axes labels don't get cut off
         if savepath is None:
             plt.show()
         else:
             plt.savefig(savepath, dpi=600)
+        plt.close()
 
 
 def calibrate(
     data_path='/Users/ruby/EoR/compact_redundant_array_sim_May2020/square_grid_sim__results.uvh5',
     model_path='/Users/ruby/EoR/compact_redundant_array_sim_May2020/square_grid_100mjy_sim_results.uvh5',
-    n_trials=100, data_stddev=.2,
+    n_trials=100,
+    data_stddev=.2,
     model_stddev_scaling=None,  # Can be list
-    calibration_style='unified',
+    calibration_styles=['unified'],
     use_covariances=True,
     simple_optimization=False,
-    create_model_errors_plot=True,
+    create_model_errors_plot=False,
     model_errors_plot_savepath=None,
+    perfect_model=False,
+    model_injected_noise_stddev_jy=None,
     quiet=False
 ):
 
-    if calibration_style == 'sky':
-        optimize_function_name = optimize_sky_cal
-    elif calibration_style == 'unified':
-        if simple_optimization:
-            optimize_function_name = optimize_unified_cal_simple
-        else:
-            optimize_function_name = optimize_unified_cal
-    elif calibration_style == 'redundant':
-        optimize_function_name = optimize_redundant_cal
-    else:
-        print(
-            'ERROR: calibration_style options are "sky", "unified", and '
-            '"redundant". Exiting.'
-        )
-        sys.exit(1)
-
-    if calibration_style != 'unified':
+    if 'unified' not in calibration_styles:
         if use_covariances:
             print(
                 'WARNING: Not using unified calibration. '
@@ -715,6 +718,20 @@ def calibrate(
                 'WARNING: Simple optimization is implemented for unified '
                 'calibration only.'
             )
+        if model_stddev_scaling is not None:
+            print(
+                'WARNING: Simple model stddev scaling is implemented for '
+                'unified calibration only.'
+            )
+            model_stddev_scaling = None
+
+    if perfect_model:
+        if model_path != data_path:
+            print(
+                'WARNING: perfect_model is set. Using file path {} for '
+                'model visibilities'.format(data_path)
+            )
+        model_path = data_path
 
     uvw_match_tolerance = 1e-12
 
@@ -732,9 +749,11 @@ def calibrate(
         convention='u>0', use_enu=False, uvw_tol=0.01
     )
 
-    baseline_groups, vec_bin_centers, lengths, conjugates = data_sim_compact.get_redundancies(
-        tol=0.1, use_antpos=False, include_conjugates=True,
-        include_autos=True, conjugate_bls=False
+    baseline_groups, vec_bin_centers, lengths, conjugates = (
+        data_sim_compact.get_redundancies(
+            tol=0.1, use_antpos=False, include_conjugates=True,
+            include_autos=True, conjugate_bls=False
+        )
     )
 
     # Define constants
@@ -835,7 +854,7 @@ def calibrate(
         gains_exp_mat_1[baseline, data_sim_expanded.ant_1_array[baseline]] = 1
         gains_exp_mat_2[baseline, data_sim_expanded.ant_2_array[baseline]] = 1
 
-    if calibration_style == 'redundant':  # Calculate antenna positions
+    if 'redundant' in calibration_styles:  # Calculate antenna positions
         ant_locs = (
             data_sim_expanded.antenna_positions
             + data_sim_expanded.telescope_location
@@ -846,6 +865,19 @@ def calibrate(
         ant_locs = ant_locs[:, :2]  # Discard altitude information
     else:
         ant_locs = None
+
+    if (
+        model_injected_noise_stddev_jy != 0
+        and model_injected_noise_stddev_jy is not None
+    ):
+        # Inject noise on the model
+        model_sim_visibilities += (
+            np.random.normal(
+                0, model_injected_noise_stddev_jy, N_red_baselines
+            ) + 1j*np.random.normal(
+                0, model_injected_noise_stddev_jy, N_red_baselines
+            )
+        )
 
     # Calculate deviation between model and true data
     model_stddev_sim = np.sqrt(np.mean(np.abs(
@@ -859,15 +891,6 @@ def calibrate(
             savepath=model_errors_plot_savepath
         )
 
-    # Generate noisy data
-    data_vis_noisy = np.zeros((N_vis, n_trials), dtype=np.complex_)
-    for trial_ind in range(n_trials):
-        data_vis_noisy[:, trial_ind] = (
-            data_sim_expanded.data_array[:, 0, 0, 0]
-            + np.random.normal(0, data_stddev, N_vis)
-            + 1j*np.random.normal(0, data_stddev, N_vis)
-        )
-
     if model_stddev_scaling is None:
         model_stddev_scaling = [1.]
     model_stddev_use_vals = [
@@ -875,44 +898,76 @@ def calibrate(
     ]
 
     # Run optimization
-    gain_vals = np.zeros(
-        (N_ants, n_trials, len(model_stddev_use_vals)), dtype=np.complex_
-    )
-    vis_diff_vals = np.zeros(
-        (N_red_baselines, n_trials, len(model_stddev_use_vals)),
-        dtype=np.complex_
-    )
+    gain_vals = np.zeros((
+        N_ants, n_trials, len(model_stddev_use_vals),
+        len(calibration_styles)
+    ), dtype=np.complex_)
+    vis_diff_vals = np.zeros((
+        N_red_baselines, n_trials, len(model_stddev_use_vals),
+        len(calibration_styles)
+    ), dtype=np.complex_)
 
-    for stddev_ind, model_stddev_use in enumerate(model_stddev_use_vals):
+    for trial in range(n_trials):
 
-        for trial in range(n_trials):
-            if not quiet:
+        # Generate noisy data
+        data_visibilities = (
+            data_sim_expanded.data_array[:, 0, 0, 0]
+            + np.random.normal(0, data_stddev, N_vis)
+            + 1j*np.random.normal(0, data_stddev, N_vis)
+        )
+
+        for style_ind, cal_style in enumerate(calibration_styles):
+
+            if cal_style == 'sky':
+                optimize_function_name = optimize_sky_cal
+            elif cal_style == 'unified':
+                if simple_optimization:
+                    optimize_function_name = optimize_unified_cal_simple
+                else:
+                    optimize_function_name = optimize_unified_cal
+            elif cal_style == 'redundant':
+                optimize_function_name = optimize_redundant_cal
+            else:
                 print(
-                    '***Version {}, Trial {}***'.format(stddev_ind+1, trial+1)
+                    'ERROR: calibration_style options are "sky", "unified", '
+                    'and "redundant". Exiting.'
                 )
-            data_visibilities = data_vis_noisy[:, trial]
+                sys.exit(1)
 
-            gains_fit, vis_fit = optimize_function_name(
-                data_visibilities, model_sim_visibilities,
-                baseline_cov_inv, a_mat, gains_exp_mat_1, gains_exp_mat_2,
-                N_red_baselines, N_ants, data_stddev, model_stddev_use,
-                ant_locs,
-                gains_init=None,
-                fitted_visibilities_init=np.matmul(
-                    np.linalg.pinv(a_mat), data_visibilities
-                ),
-                quiet=quiet
-            )
+            for stddev_ind, model_stddev_use in enumerate(
+                model_stddev_use_vals
+            ):
+                if not quiet:
+                    print('***{} Calibration, Version {}, Trial {}***'.format(
+                        cal_style, stddev_ind+1, trial+1
+                    ))
+                gains_fit, vis_fit = optimize_function_name(
+                    data_visibilities, model_sim_visibilities,
+                    baseline_cov_inv, a_mat, gains_exp_mat_1, gains_exp_mat_2,
+                    N_red_baselines, N_ants, data_stddev, model_stddev_use,
+                    ant_locs,
+                    gains_init=None,
+                    #fitted_visibilities_init=np.matmul(
+                    #    np.linalg.pinv(a_mat), data_visibilities
+                    #),
+                    fitted_visibilities_init=model_sim_visibilities,
+                    quiet=quiet
+                )
 
-            gain_vals[:, trial, stddev_ind] = gains_fit-1
-            if vis_fit is not None:
+                gain_vals[:, trial, stddev_ind, style_ind] = gains_fit
                 vis_fit_diff = vis_fit-data_sim_vis_no_noise
-                vis_diff_vals[:, trial, stddev_ind] = vis_fit_diff
+                vis_diff_vals[
+                    :, trial, stddev_ind, style_ind
+                ] = vis_fit_diff
 
     return gain_vals, vis_diff_vals
 
 
 if __name__ == '__main__':
-    gain_vals, vis_diff_vals = calibrate(n_trials=10, calibration_style='redundant')
-    histogram_plot_2d(gain_vals, gains=True)
-    histogram_plot_2d(vis_diff_vals, gains=False)
+    gain_vals, vis_diff_vals = calibrate(
+        n_trials=100
+    )
+    histogram_plot_2d(
+        gain_vals[:, :, :, 0], gains=True,
+        savepath='/Users/ruby/EoR/unified_calibration_plotting_oct2020/test_abs_cal/gains_regular_unified_cal.png'
+    )
