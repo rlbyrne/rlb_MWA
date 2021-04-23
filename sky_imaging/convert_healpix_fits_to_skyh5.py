7cf57ea6cb322f6843ab15c7c53272894d6b695d
Ruby Byrne <rlbyrne@uw.edu>
2021-04-22 17:28:48 -0700

    skyh5 file writing function

--- sky_imaging/convert_healpix_fits_to_skyh5.py
+++ sky_imaging/convert_healpix_fits_to_skyh5.py
@@ -5,9 +5,10 @@ import healpy as hp
 import sys
 import os
 import healpix_utils
+import pyradiosky
+from astropy.units import Quantity
 
-sourcedir = '/Users/rubybyrne/diffuse_survey_plotting_Aug2020'
-save_filename = '/Users/rubybyrne/diffuse_survey_plotting_Sept2020/polarized_diffuse_map.fits'
+sourcedir = '/Users/ruby/Downloads'
 pols = ['I', 'Q', 'U', 'V']
 maps = []
 for pol_ind, pol_name in enumerate(pols):
@@ -15,4 +16,31 @@ for pol_ind, pol_name in enumerate(pols):
         '{}/Stokes{}_average_map_empirical_rm_in_eor0.fits'.format(sourcedir, pol_name)
     )
     maps.append(new_map)
-healpix_utils.write_data_to_standard_fits(maps, save_filename)
+
+# Check that the pixel arrays are identical
+for pol_ind in range(len(pols)-1):
+    if np.max(np.abs(maps[pol_ind].pix_arr - maps[pol_ind+1].pix_arr)) != 0:
+        print('ERROR: Mismatched pixel arrays.')
+        sys.exit(1)
+
+Ncomponents = np.shape(maps[0].signal_arr)[0]
+stokes = np.zeros((4, 1, Ncomponents))
+for pol_ind in range(len(pols)):
+    stokes[pol_ind, :, :] = maps[pol_ind].signal_arr
+stokes = Quantity(stokes, 'Jy/sr')
+freq_array = Quantity([182000000], 'Hz')
+if maps[0].nest:
+    hpx_order = 'nested'
+else:
+    hpx_order = 'ring'
+
+skymodel = pyradiosky.SkyModel(
+    component_type='healpix',
+    spectral_type='flat',
+    stokes=stokes,
+    freq_array=freq_array,
+    hpx_inds=maps[0].pix_arr,
+    hpx_order=hpx_order,
+    nside=maps[0].nside
+)
+skymodel.write_skyh5('/Users/ruby/EoR/diffuse_map.skyh5')
