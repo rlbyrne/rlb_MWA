update and bug fix empirical rm calculation

sky_imaging/calculate_empirical_rm.py
@@ -495,7 +495,7 @@ def calculate_empirical_rm_iterative():
                 quiet=True
             )
             u_map = healpix_utils.load_map(
-                '{}/output_data/{}_weighted_Residual_Q_HEALPix.fits'.format(path, obsid),
+                '{}/output_data/{}_weighted_Residual_U_HEALPix.fits'.format(path, obsid),
                 quiet=True
             )
 
@@ -664,7 +664,7 @@ def calculate_empirical_rm_no_iteration():
             quiet=True
         )
         u_map = healpix_utils.load_map(
-            '{}/output_data/{}_weighted_Residual_Q_HEALPix.fits'.format(path, obsid),
+            '{}/output_data/{}_weighted_Residual_U_HEALPix.fits'.format(path, obsid),
             quiet=True
         )
 
@@ -812,5 +812,311 @@ def save_empirical_rm_vals_in_eor0():
     csv_outfile.close()
 
 
+def calculate_empirical_rm_Jul2021():
+
+    eor0_obslist = [
+        '1131454296',
+        '1131713632',
+        '1131710032',
+        '1131456096',
+        '1131540824',
+        '1131539024',
+        '1131538904',
+        '1131540704',
+        '1131455736',
+        '1131710392',
+        '1131708952',
+        '1131457176',
+        '1131716512',
+        '1131458976',
+        '1131712192',
+        '1131453936',
+        '1131457536',
+        '1131537704',
+        '1131543584'
+    ]
+    obslist_full = [
+        '1131551744',
+        '1130783824',
+        '1131562544',
+        '1131709912',
+        '1130776864',
+        '1131461496',
+        '1130782264',
+        '1131715432',
+        '1131733552',
+        '1131542624',
+        '1130773144',
+        '1131461376',
+        '1131557144',
+        '1131454296',
+        '1131731752',
+        '1130778664',
+        '1131470496',
+        '1131559064',
+        '1131717232',
+        '1131463536',
+        '1130773264',
+        '1131463416',
+        '1131717352',
+        '1131713632',
+        '1131478056',
+        '1131468936',
+        '1131468696',
+        '1131535424',
+        '1131463296',
+        '1131465216',
+        '1131710032',
+        '1130776624',
+        '1131456096',
+        '1131540824',
+        '1131711952',
+        '1131459576',
+        '1131477936',
+        '1131733672',
+        '1131564464',
+        '1130787784',
+        '1131461616',
+        '1131558944',
+        '1131470616',
+        '1131549944',
+        '1131553544',
+        '1131459696',
+        '1130780464',
+        '1131726352',
+        '1131470736',
+        '1131548024',
+        '1131710152',
+        '1130785864',
+        '1131544424',
+        '1131542504',
+        #'1131733432',
+        '1131735232',
+        '1131553664',
+        '1131724432',
+        '1131542744',
+        '1131455976',
+        '1131719152',
+        '1131454416',
+        '1130787544',
+        '1130776744',
+        '1130780224',
+        '1131551624',
+        '1131722632',
+        '1131547904',
+        '1131562664',
+        '1131550064',
+        '1131537104',
+        '1131555224',
+        '1131467136',
+        '1131539024',
+        '1131555344',
+        '1131546104',
+        '1131548144',
+        '1131472416',
+        '1131558824',
+        '1131544304',
+        '1130789584',
+        '1131476136',
+        '1130789344',
+        '1131722872',
+        '1130785744',
+        '1131730072',
+        '1131459816',
+        '1131564584',
+        '1131457776',
+        '1131724552',
+        '1130787664',
+        '1130778424',
+        '1131728152',
+        '1131722752',
+        '1131538904',
+        '1131544544',
+        '1130778544',
+        '1131467016',
+        '1131546344',
+        '1130789464',
+        '1131713512',
+        '1131546224',
+        '1131474336',
+        '1130782144',
+        '1131735472',
+        '1130775064',
+        '1130774824',
+        '1131720832',
+        '1130774944',
+        '1131557264',
+        '1130783944',
+        '1131472296',
+        '1131465096',
+        '1131457896',
+        '1131555464',
+        '1131562424',
+        '1131551864',
+        '1131540704',
+        '1130780344',
+        '1131731632',
+        '1131468816',
+        '1131472536',
+        '1130773024',
+        '1131474096',
+        '1131465336',
+        '1131715552',
+        '1131458016',
+        '1131540944',
+        '1131557024',
+        '1131731872',
+        '1131553424',
+        '1131560864',
+        '1130784064',
+        '1131466896',
+        '1130782024',
+        '1131560624',
+        '1131474216',
+        '1131564344',
+        '1131729952',
+        '1131560744',
+        '1130785624',
+        '1131709432',
+        '1131536624',
+        '1131536384',
+        '1131711112',
+        '1131709192',
+        '1131710992',
+        '1131453456',
+        '1131565304',
+        '1131478776',
+        '1131566504',
+        '1131565184',
+        '1131566624',
+        '1131566744',
+        '1131565064',
+        '1131567944',
+        '1131478656',
+        '1131568544',
+        '1131739432',
+        '1130788504',
+        '1130788264',
+        '1131740752',
+        '1131455736',
+        '1131710392',
+        '1131708952',
+        '1131457176',
+        '1131716512',
+        '1131458976',
+        '1131712192',
+        '1131453936',
+        '1131457536',
+        '1131537704',
+        '1131543584'
+    ]
+    start_freq_mhz = 167.
+    end_freq_mhz = 198.
+
+    original_rm_path = '/Users/rubybyrne/diffuse_survey_rm_tot.csv'
+    rm_outfile = '/Users/rubybyrne/diffuse_survey_rm_empirical_Jul2021.csv'
+    run_path = '/Volumes/Bilbo/rlb_fhd_outputs/diffuse_survey_May2021/fhd_rlb_diffuse_baseline_cut_optimal_weighting_Jun2021'
+    averaged_Q_path = '/Users/rubybyrne/diffuse_survey_plotting_Jun2021/StokesQ_average_map_first_rm_correction.fits'
+    averaged_U_path = '/Users/rubybyrne/diffuse_survey_plotting_Jun2021/StokesU_average_map_first_rm_correction.fits'
+
+    # Get RMs
+    rm_data = np.genfromtxt(
+        original_rm_path, delimiter=',', dtype=None, names=True, encoding=None
+    )
+    rms_orig = np.array([
+        rm_data['RM'][np.where(rm_data['ObsID'] == int(obsid))][0] for obsid in eor0_obslist
+    ])
+
+    # Create lookup table:
+    rot_angles_lookup, rms_lookup = create_rm_lookup_table(
+        start_freq_mhz, end_freq_mhz
+    )
+
+    rms_use = np.copy(rms_orig)
+    rot_angle_deltas_list = np.zeros(len(eor0_obslist))
+    eff_rot_angle_start = get_effective_rotation_angles(
+        rms_use, start_freq_mhz, end_freq_mhz
+    )
+
+    q_average_map = healpix_utils.load_map(averaged_Q_path)
+    u_average_map = healpix_utils.load_map(averaged_U_path)
+    q_average_map.explicit_to_implicit_ordering()
+    u_average_map.explicit_to_implicit_ordering()
+
+    # Calculate the empirical rotation angles
+    for obsind, obsid in enumerate(eor0_obslist):
+
+        q_map = healpix_utils.load_map(
+            '{}/output_data/{}_optimal_Residual_Q_HEALPix.fits'.format(run_path, obsid),
+            quiet=True
+        )
+        u_map = healpix_utils.load_map(
+            '{}/output_data/{}_optimal_Residual_U_HEALPix.fits'.format(run_path, obsid),
+            quiet=True
+        )
+
+        # Apply RM correction to maps
+        maps_rot = healpix_utils.rm_correction(
+            obsid, [None, q_map, u_map, None], rm_file=None,
+            start_freq_mhz=start_freq_mhz, end_freq_mhz=end_freq_mhz,
+            use_single_freq_calc=False, use_rm=rms_use[obsind]
+        )
+        q_map_rot = maps_rot[1]
+        u_map_rot = maps_rot[2]
+
+        # Confirm that pixel ordering matches
+        if np.sum(np.abs(q_map_rot.pix_arr-u_map_rot.pix_arr)) != 0:
+            print 'ERROR: Different pixel ordering.'
+
+        q_average_map_signal = np.array(
+            [q_average_map.signal_arr[ind] for ind in q_map_rot.pix_arr]
+        )
+        u_average_map_signal = np.array(
+            [u_average_map.signal_arr[ind] for ind in q_map_rot.pix_arr]
+        )
+
+        tangent_numerator = np.sum(
+            q_average_map_signal*u_map_rot.signal_arr - u_average_map_signal*q_map_rot.signal_arr
+        )
+        tangent_denominator = np.sum(
+            q_average_map_signal*q_map_rot.signal_arr + u_average_map_signal*u_map_rot.signal_arr
+        )
+        rot_angle_deltas_list[obsind] = np.arctan2(tangent_numerator, tangent_denominator)
+
+    # Ensure that the change in the rotation angles is mean-zero
+    mean_angle = np.arctan2(
+        np.sum(np.sin(rot_angle_deltas_list)),
+        np.sum(np.cos(rot_angle_deltas_list))
+    )
+    rot_angle_deltas_list -= mean_angle
+
+    eff_rot_angle = eff_rot_angle_start + rot_angle_deltas_list
+    # Ensure that the rotation angles are within +/- pi
+    eff_rot_angle = np.arctan2(np.sin(eff_rot_angle), np.cos(eff_rot_angle))
+
+    # Convert effective rotation angles to RMs
+    for obsind in range(len(eor0_obslist)):
+        rms_use[obsind] = interpolate_rms(
+            rms_lookup, rot_angles_lookup,
+            rms_orig[obsind], eff_rot_angle[obsind],
+            start_freq_mhz, end_freq_mhz
+        )
+
+    # Save RMs to a CSV file
+    rms_orig_all = np.array([
+        rm_data['RM'][np.where(rm_data['ObsID'] == int(obsid))][0] for obsid in obslist_full
+    ])
+
+    csv_outfile = open(rm_outfile, 'w')
+    outfile_writer = csv.writer(csv_outfile)
+    outfile_writer.writerow(['ObsID', 'RM'])
+    for obsind, obsid in enumerate(obslist_full):
+        if obsid in eor0_obslist:
+            outfile_writer.writerow([obsid, rms_use[eor0_obslist.index(obsid)]])
+        else:
+            outfile_writer.writerow([obsid, rms_orig_all[obsind]])
+    csv_outfile.close()
+
+
 if __name__=='__main__':
-    save_empirical_rm_vals_in_eor0()
+    calculate_empirical_rm_Jul2021()