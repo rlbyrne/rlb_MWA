137db288beab6b6acd9cdd269d5a44064dfa2f16
Ruby Byrne <rlbyrne@uw.edu>
2021-08-03 17:59:20 -0700

    make all jones matrix plots, reformat code

--- plot_jones_matrix.py
+++ plot_jones_matrix.py
@@ -13,82 +13,124 @@ def main():
     # function. Jones matrix is in Az/ZA coordinates.
     jones_path = '/Users/ruby/Astro/jones_matrix_plotting/zenith_jones.sav'
     jones_struct = scipy.io.readsav(jones_path)['jones']
-    obs_path = '/Users/ruby/Astro/jones_matrix_plotting/1131454296_obs.sav'
-    obs_struct = scipy.io.readsav(obs_path)['obs']
-    degpix = obs_struct['degpix'][0]
+    #obs_path = '/Users/ruby/Astro/jones_matrix_plotting/1131454296_obs.sav'
+    #obs_struct = scipy.io.readsav(obs_path)['obs']
+    #degpix = obs_struct['degpix'][0]
     deg_extent = 153.5  # Calculated by looking at the Dec. extent of the image
     dim = np.shape(jones_struct[0, 0])[0]
 
-    jones_p_amp = np.sqrt(
-        np.real(jones_struct[0, 0])**2. + np.real(jones_struct[0, 1])**2.
-    )
-    jones_p_amp[np.where(jones_p_amp == 0.)] = np.nan
-    jones_q_amp = np.sqrt(
-        np.real(jones_struct[1, 0])**2. + np.real(jones_struct[1, 1])**2.
-    )
-    jones_q_amp[np.where(jones_q_amp == 0.)] = np.nan
-    k_mat = np.zeros((2, 2, dim, dim))
+    output_path = '/Users/ruby/Astro/jones_matrix_plotting'
+
+    jones_mat = np.zeros((2, 2, dim, dim), dtype=complex)
+    for ind1 in range(2):
+        for ind2 in range(2):
+            jones_mat[ind1, ind2, :, :] = jones_struct[ind1, ind2]
+    jones_mat[np.where(jones_mat == 0.)] = np.nan
+
+    jones_amp = np.zeros((2, dim, dim))
     for ind in range(2):
-        k_mat[0, ind, :, :] = np.real((jones_struct[0, ind])/jones_p_amp)
-        k_mat[1, ind, :, :] = np.real((jones_struct[1, ind])/jones_q_amp)
-    jones_p_amp /= np.nanmax(jones_p_amp)
-    jones_q_amp /= np.nanmax(jones_q_amp)
+        jones_amp[ind, :, :] = np.sqrt(
+            np.real(jones_mat[ind, 0, :, :])**2. + np.real(jones_mat[ind, 1, :, :])**2.
+        )
+    k_mat = np.zeros((2, 2, dim, dim))
+    for ind1 in range(2):
+        for ind2 in range(2):
+            k_mat[ind1, ind2, :, :] = np.real(jones_mat[ind1, ind2, :, :]/jones_amp[ind1, :, :])
+    for ind in range(2):
+        jones_mat[ind, :, :, :] /= np.nanmax(jones_amp[ind, :, :])
+        jones_amp[ind, :, :] /= np.nanmax(jones_amp[ind, :, :])
 
-    angle_p = np.squeeze(np.arctan2(k_mat[0, 1, :, :], k_mat[0, 0, :, :]))
-    angle_q = np.squeeze(np.arctan2(k_mat[1, 1, :, :], k_mat[1, 0, :, :]))
+    # Plot elements of the Jones matrix
+    use_cmap = matplotlib.cm.get_cmap('Spectral')
+    use_cmap.set_bad(color='grey')
+    for ind1 in range(2):
+        for ind2 in range(2):
+            fig, ax = plt.subplots()
+            plt.imshow(
+                np.real(jones_mat[ind1, ind2, :, :]), origin='lower', interpolation='none',
+                vmin=-1, vmax=1,
+                cmap=use_cmap,
+                extent=[-dim/2, dim/2, -dim/2, dim/2]
+            )
+            plt.axis('off')  # Turn off axes
+            ax = add_polar_axes(ax, dim, deg_extent)
+            cbar = plt.colorbar()
+            cbar.ax.set_ylabel(
+                'Beam Response, Real Part', rotation=270, labelpad=15
+            )
+            plt.title('J[{}{}]'.format(ind1, ind2))
+            plt.savefig('{}/jones_mat_{}{}.png'.format(output_path, ind1, ind2))
 
-    plot_amp = False
-    if plot_amp:
-        plot_vals = jones_p_amp
-        use_cmap = matplotlib.cm.get_cmap('viridis')
-        use_cmap.set_bad(color='grey')
-    else:
-        plot_vals = np.zeros_like(jones_p_amp)
-        use_cmap = matplotlib.cm.get_cmap('Greys')
+    # Plot beam amplitudes
+    use_cmap = matplotlib.cm.get_cmap('viridis')
+    use_cmap.set_bad(color='grey')
+    for pol in range(2):
+        if pol == 0:
+            pol_name = 'P'
+        else:
+            pol_name = 'Q'
+        fig, ax = plt.subplots()
+        plt.imshow(
+            jones_amp[ind, :, :], origin='lower', interpolation='none',
+            vmin=0, vmax=1,
+            cmap=use_cmap,
+            extent=[-dim/2, dim/2, -dim/2, dim/2]
+        )
+        plt.axis('off')  # Turn off axes
+        ax = add_polar_axes(ax, dim, deg_extent)
+        cbar = plt.colorbar()
+        cbar.ax.set_ylabel(
+            'Beam Response Amplitude', rotation=270, labelpad=15
+        )
+        plt.title('Beam Amplitude, Polarization {}'.format(pol_name))
+        plt.savefig('{}/beam_amp_{}.png'.format(output_path, pol_name))
 
+    # Plot instrumental basis
+    use_cmap = matplotlib.cm.get_cmap('Greys')
+    plot_vals = np.zeros((dim, dim))
     fig, ax = plt.subplots()
+    # Plot zeroed data to set plot extent
     plt.imshow(
-        plot_vals, origin='lower', interpolation='none', vmin=0, vmax=1,
+        plot_vals, origin='lower', interpolation='none',
+        vmin=0, vmax=1,
         cmap=use_cmap,
         extent=[-dim/2, dim/2, -dim/2, dim/2]
     )
     plt.axis('off')  # Turn off axes
-    if plot_amp:
-        cbar = plt.colorbar()
-        cbar.ax.set_ylabel(
-            'Beam Response, Abs. Value', rotation=270, labelpad=15
-        )
 
-    if not plot_amp:
-        x_pixel_vals = np.linspace(-dim/2., dim/2., dim)
-        y_pixel_vals = np.linspace(-dim/2., dim/2., dim)
-        use_x_pixels = np.rint(np.linspace(0, dim-1, 20)).astype(int)
-        use_y_pixels = np.rint(np.linspace(0, dim-1, 20)).astype(int)
+    x_pixel_vals = np.linspace(-dim/2., dim/2., dim)
+    y_pixel_vals = np.linspace(-dim/2., dim/2., dim)
+    use_x_pixels = np.rint(np.linspace(0, dim-1, 20)).astype(int)
+    use_y_pixels = np.rint(np.linspace(0, dim-1, 20)).astype(int)
 
-        for xpix in use_x_pixels:
-            for ypix in use_y_pixels:
-                if (
-                    np.isfinite(jones_p_amp[xpix, ypix])
-                    and np.isfinite(jones_q_amp[xpix, ypix])
-                ):
-                    par_ang = np.arctan2(
-                        y_pixel_vals[ypix], x_pixel_vals[xpix]
+    for xpix in use_x_pixels:
+        for ypix in use_y_pixels:
+            if (np.isfinite(np.mean(jones_amp[:, xpix, ypix]))):
+                par_ang = np.arctan2(
+                    y_pixel_vals[ypix], x_pixel_vals[xpix]
+                )
+                for pol in range(2):
+                    plot_angle = np.arctan2(k_mat[pol, 1, xpix, ypix], -k_mat[pol, 0, xpix, ypix])+par_ang
+
+                    line_length = 50.
+                    plt.plot(
+                        [
+                            x_pixel_vals[xpix] - line_length/2.*np.cos(plot_angle),
+                            x_pixel_vals[xpix] + line_length/2.*np.cos(plot_angle)
+                        ],
+                        [
+                            y_pixel_vals[ypix] - line_length/2.*np.sin(plot_angle),
+                            y_pixel_vals[ypix] + line_length/2.*np.sin(plot_angle)
+                        ],
+                        color='black', linewidth=0.5
                     )
-                    for pol in range(2):
-                        plot_angle = np.arctan2(k_mat[pol, 1, xpix, ypix], -k_mat[pol, 0, xpix, ypix])+par_ang
+    ax = add_polar_axes(ax, dim, deg_extent)
 
-                        line_length = 50.
-                        plt.plot(
-                            [
-                                x_pixel_vals[xpix] - line_length/2.*np.cos(plot_angle),
-                                x_pixel_vals[xpix] + line_length/2.*np.cos(plot_angle)
-                            ],
-                            [
-                                y_pixel_vals[ypix] - line_length/2.*np.sin(plot_angle),
-                                y_pixel_vals[ypix] + line_length/2.*np.sin(plot_angle)
-                            ],
-                            color='black', linewidth=0.5
-                        )
+    plt.title('Instrumental Basis')
+    plt.savefig('{}/instr_basis.png'.format(output_path))
+
+
+def add_polar_axes(ax, dim, deg_extent):
 
     # Plot azimuth lines
     azimuth_lines = 8
@@ -115,7 +157,7 @@ def main():
                 annotation_loc[ind] = dim/2-annotate_pad_from_edge
             elif annotation_loc[ind] < -(dim/2-annotate_pad_from_edge):
                 annotation_loc[ind] = -(dim/2-annotate_pad_from_edge)
-        plt.annotate(
+        ax.annotate(
             '${}^\circ$'.format(round(line_ang)),
             (annotation_loc[0], annotation_loc[1]),
             horizontalalignment='center', verticalalignment='center'
@@ -136,13 +178,13 @@ def main():
         annotation_loc = (circ_radius+annotate_pad)*np.array([
             np.cos(np.radians(annotate_ang)), np.sin(np.radians(annotate_ang))
         ])
-        plt.annotate(
+        ax.annotate(
             '${}^\circ$'.format(round(za)),
             (annotation_loc[0], annotation_loc[1]),
             horizontalalignment='center', verticalalignment='center'
         )
 
-    plt.show()
+    return ax
 
 
 if __name__ == '__main__':
